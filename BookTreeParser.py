739cea1ba6c16420b231bb876fc46c46f14ed4e1
tomjw64 <tom.jw64@gmail.com>
2017-06-06 17:47:58 -0500

    Add basic sentence separation

--- BookTreeParser.py
+++ BookTreeParser.py
@@ -39,7 +39,6 @@ def enum(*sequential, **named):
     enums['to_string'] = reverse
     return type('Enum', (), enums)
 
-# TODO: Add SECTION (within chapter)
 NodeType = enum('BOOK', 'UNIT', 'CHAPTER', 'PAGE', 'SECTION', 'FEATURE', 'TEXT')
 node_type_assign = {
   'note' : NodeType.FEATURE,
@@ -67,14 +66,17 @@ class BookTreeNode(object):
   def add_child(self, child):
     self.children.append(child)
 
+  def rm_child(self, child):
+    self.children.remove(child)
+
   def print_as_root(self, level=0):
-    if self.node_type != NodeType.TEXT:
-      print "#",
-      for _ in range(level):
-        print "|",
-      print self
-      for child in self.children:
-        child.print_as_root(level + 1)
+    #if self.node_type != NodeType.TEXT:
+    print "#",
+    for _ in range(level):
+      print "|",
+    print self
+    for child in self.children:
+      child.print_as_root(level + 1)
 
   def __repr__(self):
     return "<BOOK_NODE node_type=" + NodeType.to_string[self.node_type] + " cargo=" + str(self.cargo) + ">"
@@ -87,7 +89,10 @@ class BookTreeParser(HTMLParser):
     self.relevant_levels = [0]
     self.tree_parent_stack = Stack()
     self.titling = False
+    self.within_metadata = False
     self.last_sibling = None
+
+    self.text_buffer = ''
 
     book_root = BookTreeNode(None, None, NodeType.BOOK, None)
     self.root = book_root
@@ -95,26 +100,55 @@ class BookTreeParser(HTMLParser):
 
   def handle_starttag(self, tag, attrs):
     self.full_parent_level += 1
-    if tag == 'title':
-      self.titling = True
-    elif tag in ('div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'section'):
-      if tag == 'section':
-        attrs.append(('data-type', 'section'))
-      for attr_pair in attrs:
-        if attr_pair[0] == 'data-type':
-          data_type = attr_pair[1]
-          if data_type in node_type_assign:
-            self.relevant_levels.append(self.full_parent_level)
-            node_type = node_type_assign[data_type]
-            opened_node = BookTreeNode(self.tree_parent_stack.peek(), self.last_sibling, node_type, None)
-            self.tree_parent_stack.push(opened_node)
-            self.last_sibling = None
-          elif data_type in ('document-title', 'title'):
-            self.titling = True
+    if not self.within_metadata:
+      if tag == 'title':
+        self.titling = True
+      elif tag in ('div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'section'):
+        if tag == 'section':
+          attrs.append(('data-type', 'section'))
+        for attr_pair in attrs:
+          if attr_pair[0] == 'data-type':
+            data_type = attr_pair[1]
+            if data_type == 'metadata':
+              self.within_metadata = True
+              self.relevant_levels.append(self.full_parent_level)
+            elif data_type in node_type_assign:
+
+              # Just in case
+              self.titling = False
+
+              self.relevant_levels.append(self.full_parent_level)
+              node_type = node_type_assign[data_type]
+              opened_node = BookTreeNode(self.tree_parent_stack.peek(), self.last_sibling, node_type, None)
+              self.tree_parent_stack.push(opened_node)
+
+              if self.last_sibling is not None and self.last_sibling.node_type == NodeType.TEXT:
+                common_parent = self.last_sibling.parent
+                sentences = re.findall('[^?.]*?[.?]', self.last_sibling.cargo)
+                prev_it_sibling = self.last_sibling.sibling_prev
+                for nth_sent in sentences:
+                  current_sentence = BookTreeNode(common_parent, prev_it_sibling, NodeType.TEXT, nth_sent)
+                  prev_it_node = current_sentence
+                common_parent.rm_child(self.last_sibling)
+
+              self.last_sibling = None
+            elif data_type in ('document-title', 'title'):
+              self.titling = True
 
   def handle_endtag(self, tag):
     if self.full_parent_level == self.relevant_levels[-1]:
-      self.last_sibling = self.tree_parent_stack.pop()
+      if self.within_metadata:
+        self.within_metadata = False
+      else:
+        if self.last_sibling is not None and self.last_sibling.node_type == NodeType.TEXT:
+          common_parent = self.last_sibling.parent
+          sentences = self.last_sibling.cargo.split('.')
+          prev_it_sibling = self.last_sibling.sibling_prev
+          for nth_sent in sentences:
+            current_sentence = BookTreeNode(common_parent, prev_it_sibling, NodeType.TEXT, nth_sent)
+            prev_it_node = current_sentence
+          common_parent.rm_child(self.last_sibling)
+        self.last_sibling = self.tree_parent_stack.pop()
       del self.relevant_levels[-1]
     self.full_parent_level -= 1
 
@@ -126,16 +160,22 @@ class BookTreeParser(HTMLParser):
     # TODO: instead of immediately parsing into nodes, store in memory until surrounding text nodes are also parsed
     # We want to prevent something like <p> Some text <span> within </span> the same sentence. </p> from becoming a problem
     # May also help with entity-ref TODO
-    if not data.isspace():
-      text = re.sub( '\s+', ' ', data)
-      parent = self.tree_parent_stack.peek()
-      if self.titling:
-        if parent.cargo is None and parent.node_type != NodeType.FEATURE:
-          parent.cargo = text
-          self.titling = False
-      else:
-        current_node = BookTreeNode(parent, self.last_sibling, NodeType.TEXT, text)
-        self.last_sibling = current_node
+    if not self.within_metadata:
+      if not data.isspace():
+        text = re.sub( '\s+', ' ', data)
+        parent = self.tree_parent_stack.peek()
+        if self.titling:
+          if parent.cargo is None and parent.node_type != NodeType.FEATURE:
+            parent.cargo = text
+            self.titling = False
+          elif parent.node_type == NodeType.FEATURE:
+            self.titling = False
+        else:
+          if self.last_sibling is not None and self.last_sibling.node_type == NodeType.TEXT:
+            self.last_sibling.cargo += text
+          else:
+            current_node = BookTreeNode(parent, self.last_sibling, NodeType.TEXT, text)
+            self.last_sibling = current_node
   
   def handle_entityref(self, data):
     # TODO: Convert to unicode and append into surrounding text
